cf2153620fe52f0f1e890cd2f6abf86f4c59b28f
Listonian <58514899+Listonian@users.noreply.github.com>
2021-03-01 03:59:39 -0800

    Update Main.java
    Edit contact info

--- ianListonIndividualProjectBurger211Update/src/ianListonIndividualProject/Burger211Update/Main.java
+++ ianListonIndividualProjectBurger211Update/src/ianListonIndividualProject/Burger211Update/Main.java
@@ -1,8 +1,7 @@
 package ianListonIndividualProject.Burger211Update;
 
 //Name: Ian Liston
-//Contact info: ian.liston@bellevuecollege.edu
-//Student ID: 950457388
+//Contact info: Listonian@ymail.com
 //Date: 11/30/2020
 //Description: Class main runs classes Burger211, ExchangeRate, Franchise, MenuGUI, and GetInputGUI.
 //           
